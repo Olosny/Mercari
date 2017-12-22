a0ba852dd8cd8d92f4202a1ab0a949f312a73d4a
jeremiedbb <jeremiedbb@yahoo.fr>
2017-12-22 19:23:52 +0100

    60K

--- 60K.py
+++ 60K.py
@@ -23,7 +23,11 @@ df_merc.drop(columns = ['category_name', 'name', 'item_description'], inplace = 
 # Hard clean
 df_merc.dropna(axis=0, how='any', inplace=True)
 
-# Get dummies
+# Group low frequent brand_name
+brand_counts = df_merc['brand_name'].value_counts().to_frame()
+df_merc['brand_name'][df_merc['brand_name'].isin(brand_counts[brand_counts['brand_name']<10].index)] = 'Rare_brands'
+
+# categorical to numeric
 df_merc_cat = df_merc
 cols = ['brand_name','first_cat','second_cat','third_cat']
 for col in cols:
@@ -31,16 +35,19 @@ for col in cols:
     df_merc_cat[col] = df_merc[col].cat.codes
 
 
-# first regression
-"""estimator = RandomForestRegressor(random_state=0, n_estimators=20, n_jobs=-1, verbose=1)
-score = cross_val_score(estimator, df_merc_cat.drop(columns = 'price'), df_merc_cat.price, n_jobs=-1)
-print(score)
+df_dummies = pd.get_dummies(df_merc, columns=['brand_name','first_cat','second_cat','third_cat'])
 
-estimator.fit(df_merc_cat.drop(columns = 'price'), df_merc_cat.price)
-df_merc_cat['predicted'] = estimator.predict(df_merc_cat.drop(columns = 'price'))
-df_merc_cat['eval'] = np.power(np.log(df_merc_cat['predicted'] + 1) - np.log(df_merc_cat['price'] + 1), 2)
-eval1 = np.sqrt(1 / len(df_merc_cat['eval']) * df_merc_cat['eval'].sum())
-print(eval1)"""
+
+# first regression
+estimator = RandomForestRegressor(random_state=0, n_estimators=20, n_jobs=-1, verbose=1)
+#score = cross_val_score(estimator, df_dummies.drop(columns = 'price'), df_dummies.price, n_jobs=-1)
+#print(score)
+
+estimator.fit(df_dummies.drop(columns = 'price'), df_dummies.price)
+df_dummies['predicted'] = estimator.predict(df_dummies.drop(columns = 'price'))
+df_dummies['eval'] = np.power(np.log(df_dummies['predicted'] + 1) - np.log(df_dummies['price'] + 1), 2)
+eval1 = np.sqrt(1 / len(df_dummies['eval']) * df_dummies['eval'].sum())
+print(eval1)
 
 # 2nd
 """estimator2 = ExtraTreesRegressor(random_state=0, n_estimators=20, n_jobs=-1, verbose=1)
