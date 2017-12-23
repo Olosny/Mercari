d24748bb28e7335a268c7d40bc5ee2a766e3931a
Ivan PANICO <ivpanico@gmail.com>
2017-12-23 23:40:50 +0100

    Init of SIGKDD

--- 60K.py
+++ 60K.py
@@ -9,6 +9,22 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.linear_model import Ridge
 
+# Functions
+
+# weighing function
+def weighing(n, k, f):
+    return (1 / (1 + np.exp(- (n - k) / f)))
+
+# Handling High Cardinality Categorical Features
+def ev_cat(n, ev_loc, ev_glob):
+    k = 100
+    f = 100
+    lamb = weighing(n, k, f)
+    return (lamb * ev_loc + (1 - lamb) * ev_glob)
+
+def calculate_cat(df):
+    return ev_cat(df["size"], df["mean"], ev_price)
+
 # Read and clean
 df_merc = pd.read_table('./train.tsv', index_col = 0)
 df_merc.drop(df_merc[df_merc.price == 0].index, inplace = True)
@@ -23,31 +39,38 @@ df_merc.drop(columns = ['category_name', 'name', 'item_description'], inplace = 
 # Hard clean
 df_merc.dropna(axis=0, how='any', inplace=True)
 
+# Mapping Cat to their EV
+ev_and_count_by_brand = df_merc.groupby('brand_name').price.aggregate(['size', 'mean'])
+ev_price = df_merc.price.mean()
+brand_cortable = ev_and_count_by_brand.apply(calculate_cat, axis = 1)
+df_merc_test.brand_name.replace(brand_cortable, inplace = True)
+
+
 # Group low frequent brand_name
-brand_counts = df_merc['brand_name'].value_counts().to_frame()
-df_merc['brand_name'][df_merc['brand_name'].isin(brand_counts[brand_counts['brand_name']<10].index)] = 'Rare_brands'
+#brand_counts = df_merc['brand_name'].value_counts().to_frame()
+#df_merc['brand_name'][df_merc['brand_name'].isin(brand_counts[brand_counts['brand_name']<1000].index)] = 'Rare_brands'
 
 # categorical to numeric
-df_merc_cat = df_merc
-cols = ['brand_name','first_cat','second_cat','third_cat']
-for col in cols:
-    df_merc[col] = pd.Categorical(df_merc[col])
-    df_merc_cat[col] = df_merc[col].cat.codes
-
-
-df_dummies = pd.get_dummies(df_merc, columns=['brand_name','first_cat','second_cat','third_cat'])
-
-
-# first regression
-estimator = RandomForestRegressor(random_state=0, n_estimators=20, n_jobs=-1, verbose=1)
-#score = cross_val_score(estimator, df_dummies.drop(columns = 'price'), df_dummies.price, n_jobs=-1)
-#print(score)
-
-estimator.fit(df_dummies.drop(columns = 'price'), df_dummies.price)
-df_dummies['predicted'] = estimator.predict(df_dummies.drop(columns = 'price'))
-df_dummies['eval'] = np.power(np.log(df_dummies['predicted'] + 1) - np.log(df_dummies['price'] + 1), 2)
-eval1 = np.sqrt(1 / len(df_dummies['eval']) * df_dummies['eval'].sum())
-print(eval1)
+#df_merc_cat = df_merc
+#cols = ['brand_name','first_cat','second_cat','third_cat']
+#for col in cols:
+#    df_merc[col] = pd.Categorical(df_merc[col])
+#    df_merc_cat[col] = df_merc[col].cat.codes
+#
+#
+#df_dummies = pd.get_dummies(df_merc, columns=['brand_name','first_cat','second_cat','third_cat'])
+#
+#
+## first regression
+#estimator = RandomForestRegressor(random_state=0, n_estimators=20, n_jobs=-1, verbose=1)
+##score = cross_val_score(estimator, df_dummies.drop(columns = 'price'), df_dummies.price, n_jobs=-1)
+##print(score)
+#
+#estimator.fit(df_dummies.drop(columns = 'price'), df_dummies.price)
+#df_dummies['predicted'] = estimator.predict(df_dummies.drop(columns = 'price'))
+#df_dummies['eval'] = np.power(np.log(df_dummies['predicted'] + 1) - np.log(df_dummies['price'] + 1), 2)
+#eval1 = np.sqrt(1 / len(df_dummies['eval']) * df_dummies['eval'].sum())
+#print(eval1)
 
 # 2nd
 """estimator2 = ExtraTreesRegressor(random_state=0, n_estimators=20, n_jobs=-1, verbose=1)
