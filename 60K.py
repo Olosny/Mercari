d5291bc6a80b6cc168e3926a845f220698292349
jeremiedbb <jeremiedbb@yahoo.fr>
2017-12-26 01:35:05 +0100

    .

--- 60K.py
+++ 60K.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.ensemble import AdaBoostRegressor
@@ -17,12 +18,13 @@ from sklearn.linear_model import ElasticNet
 from sklearn.svm import SVR
 from sklearn.svm import NuSVR
 
+
 # Functions
-# weighing function
+ # weighing function
 def weighing(n, k, f):
     return (1 / (1 + np.exp(- (n - k) / f)))
 
-# Handling High Cardinality Categorical Features
+ # Handling High Cardinality Categorical Features
 def ev_cat(n, ev_loc, ev_glob):
     k = 100
     f = 10
@@ -32,11 +34,14 @@ def ev_cat(n, ev_loc, ev_glob):
 # Read and clean
 df_merc = pd.read_table('./train.tsv', index_col = 0)
 df_merc.drop(df_merc[df_merc.price == 0].index, inplace = True)
+
 df_cats = df_merc.category_name.str.split('/')
 cats = ['first_cat', 'second_cat', 'third_cat']
 
 for i in range(3):
     df_merc[cats[i]] = df_cats.str.get(i)
+
+
 
 
 # Hard clean
@@ -75,26 +80,26 @@ df_merc.drop(columns = ['category_name', 'name', 'item_description','first_cat',
 
 # Regression
 estimators = []
-"""estimators.append(RandomForestRegressor(n_estimators=20, n_jobs=-1,verbose=1))
-estimators.append(ExtraTreesRegressor(n_estimators=20, n_jobs=-1,verbose=1))
-estimators.append(AdaBoostRegressor())
-estimators.append(BaggingRegressor(n_estimators=10,n_jobs=-1,verbose=True))
-estimators.append(GradientBoostingRegressor(n_estimators=20, verbose=1))
-estimators.append(Ridge())
-estimators.append(SGDRegressor())
-estimators.append(Lasso())
-estimators.append(ElasticNet())
-estimators.append(SVR())"""
-estimators.append(NuSVR())
+#estimators.append(RandomForestRegressor(n_estimators=20, n_jobs=-1,verbose=1))
+#estimators.append(ExtraTreesRegressor(n_estimators=20, n_jobs=-1,verbose=1))
+#estimators.append(AdaBoostRegressor())
+#estimators.append(BaggingRegressor(n_estimators=10,n_jobs=-1,verbose=True))
+#estimators.append(GradientBoostingRegressor(n_estimators=20, verbose=1))
+#estimators.append(Ridge())
+#estimators.append(SGDRegressor())
+#estimators.append(Lasso())
+#estimators.append(ElasticNet())
+#estimators.append(SVR(verbose=True))
+#estimators.append(NuSVR(verbose=True))
 
 
 for est in estimators:
+    train, test = train_test_split(df_merc, test_size=0.2)
     print("estimator: ", est.__class__.__name__)
     print("params: ", est.get_params())
-    est.fit(df_merc.drop(columns = 'price'), df_merc.price)
-    df_merc['predicted'] = est.predict(df_merc.drop(columns = 'price'))
-    df_merc['eval'] = (df_merc['predicted'] - df_merc['price'])**2
-    eval1 = np.sqrt(1 / len(df_merc['eval']) * df_merc['eval'].sum())
+    est.fit(train.drop(columns = 'price'), train.price)
+    test['predicted'] = est.predict(test.drop(columns = 'price'))
+    test['eval'] = (test['predicted'] - test['price'])**2
+    eval1 = np.sqrt(1 / len(test['eval']) * test['eval'].sum())
     print("score: ", eval1)
-    df_merc.drop(columns=['predicted','eval'], inplace=True)
 
