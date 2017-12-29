b8c4022f90ae5af6f6f64310d8f0ff40c6e10d6a
Ivan PANICO <ivpanico@gmail.com>
2017-12-29 02:10:01 +0100

    with //

--- 60K.py
+++ 60K.py
@@ -3,6 +3,8 @@
 # Imports
 ## del warnings
 import warnings
+import datetime
+import gc
 warnings.filterwarnings("ignore")
 ## basics
 import pandas as pd
@@ -26,6 +28,7 @@ from sklearn.linear_model import SGDRegressor
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.porter import PorterStemmer
+from joblib import Parallel, delayed
 #from collections import Counter
 #import re
 from gensim import corpora, models, matutils
@@ -41,6 +44,13 @@ MAX_CAT_WORDS = 5000     # Completly random...
 LAMBDA_K = 10            # tune EV weighted function
 LAMBDA_F = 1             #          //
 FUNC = 'mean'          # function to use in high-level categorical features processing
+stop = set(stopwords.words('english'))
+more_stop = ['i\'d', 'i\'m', 'i\'ll', ';)', '***', '**', ':)', '(:', '(;',
+        ':-)', '//']
+
+for i in more_stop:
+    stop.add(i)
+
 
 
 ###############
@@ -106,32 +116,70 @@ def merge_EV(train, test, cats, keep_all=False):
     else:
         return train[all_cats], test[all_cats]
  
+# For parallel
+#def get_tokens(i,regex):
+#    #stem_mono_tokens = Parallel(n_jobs=4)(delayed(p_stemmer.stem)(j) for j in i if j not in stop)
+#    p_stemmer = PorterStemmer()
+#    tokenizer = RegexpTokenizer(regex)
+#    stem_mono_tokens = [p_stemmer.stem(j) for j in tokenizer.tokenize(i.lower()) if j not in stop]
+#    return [' '.join(i) for i in zip(stem_mono_tokens, stem_mono_tokens[1:])] + stem_mono_tokens
 
-## corpus2csc
-def csc_from_col(df, col, regex, max_voc): # Only Monograms for now
-    stop = set(stopwords.words('english'))
-    more_stop = ['i\'d', 'i\'m', 'i\'ll', ';)', '***', '**', ':)', '(:', '(;',
-            ':-)', '//']
-
-    for i in more_stop:
-        stop.add(i)
-
+def build_corpus(i,regex):
+    #stem_mono_tokens = Parallel(n_jobs=4)(delayed(p_stemmer.stem)(j) for j in i if j not in stop)
     p_stemmer = PorterStemmer()
     tokenizer = RegexpTokenizer(regex)
-    #stem_tokens = []
+    stem_mono_tokens = [p_stemmer.stem(j) for j in tokenizer.tokenize(i.lower()) if j not in stop]
+    stem_tokens = [' '.join(i) for i in zip(stem_mono_tokens, stem_mono_tokens[1:])] + stem_mono_tokens
+    return corpora.hashdictionary.HashDictionary(stem_tokens, id_range = 20000000).doc2bow(stem_tokens)
+
+#def my_dic_merge(dic_list):
+#    dic_len = len(dic_list[0])
+#    a = dic_list[0]
+#    for b in dic_list[1:]:
+#        for k in b.token2id:
+#            if k not in set(a.token2id):
+#                a.token2id[k] = dic_len
+#                dic_len += 1
+#
+#    return a
+
+## corpus2csc
+def csc_from_col(df, col, regex, max_voc, tfidf = False): # Only Monograms for now
+    #chunk_num = 3
+    #p_stemmer = PorterStemmer()
+    #tokenizer = RegexpTokenizer(regex)
+    #print("gettokens start : " + str(datetime.datetime.now().time()))
+    #stem_tokens = Parallel(n_jobs=3)(delayed(get_tokens)(i, regex) for i in list(df[col]))
+    #print("gettokens end start dictionary: " + str(datetime.datetime.now().time()))
+    #stem_tokens = [get_tokens(tokenizer.tokenize(i.lower())) for i in list(df[col])]
     #for i in list(df[col]):
     #    stem_mono_tokens = [p_stemmer.stem(j) for j in tokenizer.tokenize(i.lower()) if j not in stop]
     #    stem_tokens.append([' '.join(i) for i in zip(stem_mono_tokens, stem_mono_tokens[1:])] + stem_mono_tokens)
-    raw_tokens =[tokenizer.tokenize(i.lower()) for i in list(df[col])]
-    stopped_tokens = [[i for i in token if i not in stop] for token in raw_tokens]
-    stem_mono_tokens = [[p_stemmer.stem(i) for i in token] for token in stopped_tokens]
-    stem_tokens = [[' '.join(i) for i in zip(tokens, tokens[1:])] + tokens for tokens in stem_mono_tokens]
-    dictionary = corpora.Dictionary(stem_tokens)
-    dictionary.filter_extremes(keep_n = max_voc)
-    corpus = [dictionary.doc2bow(text) for text in stem_tokens]
-    tfidf = models.TfidfModel(corpus)
-    tfidf_corpus = tfidf[corpus]
-    return matutils.corpus2csc(tfidf_corpus).transpose()
+    #raw_tokens =tokenizer.tokenize(i.lower()) for i in list(df[col])]
+    #stopped_tokens = [[i for i in token if i not in stop] for token in raw_tokens]
+    #stem_mono_tokens = [[p_stemmer.stem(i) for i in token] for token in stopped_tokens]
+    #stem_tokens = [[' '.join(i) for i in zip(tokens, tokens[1:])] + tokens for tokens in stem_mono_tokens]
+    #gc.collect()
+    #tok_chunk = [stem_tokens[i::chunk_num] for i in range(chunk_num)]
+    #dictionary = corpora.hashdictionary.HashDictionary(stem_tokens)
+    #dictionaries = Parallel(n_jobs=3)(delayed(corpora.Dictionary)(tok) for tok in tok_chunk)
+    #print("dictionary end start dictionary_merge: " + str(datetime.datetime.now().time()))
+    #dictionary = my_dic_merge(dictionaries)
+    #print("dictionary_merge end start dictionary_filter: " + str(datetime.datetime.now().time()))
+    #dictionary.filter_extremes(keep_n = max_voc)
+    print("dictionary_filter end start corpus: " + str(datetime.datetime.now().time()))
+    #corpus = [hashdictionary.HashDictionary(text).doc2bow(text) for text in stem_tokens]
+    corpus = Parallel(n_jobs = 3)(delayed(build_corpus)(i, regex) for i in list(df[col]))
+    if tfidf:
+        tfidf = models.TfidfModel(corpus)
+        tfidf_corpus = tfidf[corpus]
+        print("corpus end start return: " + str(datetime.datetime.now().time()))
+        my_csc_matrix = matutils.corpus2csc(tfidf_corpus).transpose()
+    else:
+        print("corpus end start return: " + str(datetime.datetime.now().time()))
+        my_csc_matrix = matutils.corpus2csc(corpus).transpose()
+    return my_csc_matrix[:, my_csc_matrix.getnnz(0) > 400]
+    #return matutils.corpus2csc(tfidf_corpus).transpose()
 
 
 ##########
@@ -139,15 +187,16 @@ def csc_from_col(df, col, regex, max_voc): # Only Monograms for now
 ##########
 
 # Read and clean
-print("Read and PrePreprocessing")
+print("Read and PrePreprocessing : " + str(datetime.datetime.now().time()))
 
 ## Read train
 df_train = pd.read_table('./train.tsv', index_col = 0)
+#df_train = pd.read_table('../input/train.tsv', index_col = 0)             # for kaggle kernel
 df_train['price'] = np.log(df_train['price']+1)  # Price -> Log
 df_train = df_train[df_train['price'] != 0]      # drop price == 0$
 
 ## Read test
-#df_test = pd.read_table('./test.tsv', index_col = 0)
+#df_test = pd.read_table('../input/test.tsv', index_col = 0)
 
 ### if run only on train ###
 df_train, df_test = train_test_split(df_train, test_size=0.3)
@@ -156,46 +205,49 @@ split_index = len(df_train)
 whole = pd.concat([df_train, df_test])
 na_remove(whole)                   # fill nans
 
-print("Finished")
+print("Finished : " + str(datetime.datetime.now().time()))
 ####################################
 
 
 # Preprocessing categorical columns
-print("Begin Preprocessing")
+print("Begin Preprocessing : " + str(datetime.datetime.now().time()))
 
 ## Item Description
-print("Begin description preprocessing")
-csc_desc = csc_from_col(whole, 'item_description', r'\w+', MAX_DESC_WORDS)
-print("End description preprocessing")
+print("Begin description preprocessing : " + str(datetime.datetime.now().time()))
+csc_desc = csc_from_col(whole, 'item_description', r'\w+', MAX_DESC_WORDS, tfidf = True)
+print("End description preprocessing : " + str(datetime.datetime.now().time()))
 
 ## Name
-print("Begin name preprocessing")
+print("Begin name preprocessing : " + str(datetime.datetime.now().time()))
 csc_name = csc_from_col(whole, 'name', r'\w+', MAX_NAME_WORDS)
-print("End name preprocessing")
+print("End name preprocessing : " + str(datetime.datetime.now().time()))
 
 ## Brand Name
-print("Begin name preprocessing")
+print("Begin name preprocessing : " + str(datetime.datetime.now().time()))
 csc_brand = csc_from_col(whole, 'brand_name', r'\w+', MAX_BRAND_WORDS)
 #train_brand, test_brand = merge_EV(df_train, df_test, ['brand_name'])
 #csc_brand = csc_matrix(pd.concat([train_brand, test_brand]))
-print("End name preprocessing")
+print("End name preprocessing : " + str(datetime.datetime.now().time()))
 
 ## Category Name
-print("Begin category preprocessing")
+print("Begin category preprocessing : " + str(datetime.datetime.now().time()))
 csc_cat = csc_from_col(whole, 'category_name', r'(?:[^/]|//)+', MAX_CAT_WORDS)
 #train_cat, test_cat = merge_EV(df_train, df_test, ['category_name'])
 #csc_cat = csc_matrix(pd.concat([train_cat, test_cat]))
-print("End category preprocessing")
+print("End category preprocessing : " + str(datetime.datetime.now().time()))
 
 ## Shipping and Item Condition
-print("Begin shipping and condition preprocessing")
+print("Begin shipping and condition preprocessing : " + str(datetime.datetime.now().time()))
 csc_ship_cond = csc_matrix(pd.get_dummies(whole[['shipping', 'item_condition_id']], sparse = True))
 #csc_ship_cond = whole[['shipping','item_condition_id']]
-print("End shipping and condition preprocessing")
+print("End shipping and condition preprocessing : " + str(datetime.datetime.now().time()))
 
 ## Final csc
 csc_final = hstack((csc_name, csc_brand, csc_cat, csc_ship_cond, csc_desc))
-print('End Preprocessing')
+print("csc_final shape : " + str(csc_final.shape))
+print("csc_final non zero : " + str(csc_final.count_nonzero()))
+print("csc_final sparsity : " + str(csc_final.count_nonzero()/(csc_final.shape[0]*csc_final.shape[1])))
+print("End Preprocessing : " + str(datetime.datetime.now().time()))
 ######################################
 
 
